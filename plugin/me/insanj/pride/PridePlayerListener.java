3ad3cbfb6c76300cd414b5e04964499e234df646
Julian Weiss <julian@openmessage.io>
2019-02-13 17:14:06 -0500

    begin work on POC next version

--- plugin/me/insanj/pride/PridePlayerListener.java
+++ plugin/me/insanj/pride/PridePlayerListener.java
@@ -19,15 +19,28 @@ public class PridePlayerListener implements Listener {
         plugin.getLogger().info(event.getPlayer().getName() + " joined the server! :D");
     }
 
-    @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        plugin.getLogger().info(event.getPlayer().getName() + " left the server! :'(");
-    }
+    /*@EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) { }
+    */
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Location from = event.getFrom();
         Location to = event.getTo();
         plugin.getLogger().info(String.format("From %.2f,%.2f,%.2f to %.2f,%.2f,%.2f", from.getX(), from.getY(), from.getZ(), to.getX(), to.getY(), to.getZ()));
+    }
+
+    public void detectPride() {
+        // purpose: use a given location to determine if a player has entered/exited an area
+        Location playerLocation; // current location
+        Location[] playerAreaHistory; // last time this player was detected within an area
+        Location[] prideAreas; // detection areas
+        Float threshold = 200; // how close you have to be to an area to activate it
+
+        // implementation: go through areas and see if playerLocation is within 200 pixels of a prideArea
+
+        // result: send a message to the server if a player has entered an area
+
+
     }
 }
