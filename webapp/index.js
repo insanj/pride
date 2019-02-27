10703e35cd4f6336faaedcec1432f0f022b61e89
insanj <me@insanj.com>
2019-02-27 05:50:41 -0500

    make progress on upload API for screenshots

--- webapp/index.js
+++ webapp/index.js
@@ -8,6 +8,7 @@ const path = require('path')
 const PromiseFtp = require('promise-ftp')
 const fs = require('fs')
 require('dotenv').config()
+const fileUpload = require('express-fileupload');
 
 // core vars
 const app = express()
@@ -19,10 +20,12 @@ const ftpConfig = {
 // routes
 app.use(express.static('static'))
 
+// -- homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+// -- download API
 app.get("/download", (req, res) => {
     // let configYAML = YAML.load(__dirname + '/config.yml');
     var ftp = new PromiseFtp();
@@ -39,5 +42,31 @@ app.get("/download", (req, res) => {
     });
 });
 
+// -- upload API
+// default options
+app.use(fileUpload());
+app.post('/upload', function(req, res) {
+    let sampleFile;
+    let uploadPath;
+  
+    if (Object.keys(req.files).length == 0) {
+      res.status(400).send('No files were uploaded.');
+      return;
+    }
+  
+    console.log('req.files >>>', req.files); // eslint-disable-line
+  
+    sampleFile = req.files.sampleFile;
+  
+    uploadPath = __dirname + '/static/' + sampleFile.name;
+  
+    sampleFile.mv(uploadPath, function(err) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+  
+      res.send('File uploaded to ' + uploadPath);
+    });
+  });
 // engage!
 app.listen(port, () => console.log(`pride webapp live @ ${port} with config ${JSON.stringify(ftpConfig)}`))
