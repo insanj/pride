eb72799d6f4b6dd97766acd7a99091bc2a052fda
insanj <me@insanj.com>
2019-02-28 19:48:28 -0500

    major refactor, reorg static dirs, bring external local

---
+++ webapp/scripts/pride_interactions.js
@@ -0,0 +1,64 @@
+// pride webapp UX
+// (c) 2019 julian weiss
+
+function pride_searchOnKeyUp() {
+    // Declare variables 
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("pride-search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("pride-table");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("th")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        } 
+    }
+}
+
+function prideSetupSections() {
+    /*$(document).on("submit", "#collapse-form", function(event) { 
+        const originalSource = $(".pride-artwork").attr('src');
+        $(".pride-artwork").attr("src", originalSource + "?" + (new Date()).getTime()); // force reload images
+    });*/
+
+    // click listeners
+    function prideToggleSection(sectionName) {
+        if (sectionName === 'names') {
+            $("#pride-names-button").removeClass("btn-outline-secondary"); 
+            $("#pride-names-button").addClass("btn-secondary"); 
+            $("#pride-names-button").prop("disabled", true); 
+
+            $("#pride-gallery-button").removeClass("btn-secondary"); 
+            $("#pride-gallery-button").addClass("btn-outline-secondary"); 
+            $("#pride-gallery-button").prop("disabled", false); 
+
+            $("#pride-table-body").html(pride_namesSectionContents);
+        } else if (sectionName === 'gallery') {
+            $("#pride-names-button").removeClass("btn-secondary"); 
+            $("#pride-names-button").addClass("btn-outline-secondary"); 
+            $("#pride-names-button").prop("disabled", false); 
+
+            $("#pride-gallery-button").removeClass("btn-outline-secondary"); 
+            $("#pride-gallery-button").addClass("btn-secondary"); 
+            $("#pride-gallery-button").prop("disabled", true);
+            
+            $("#pride-table-body").html(pride_gallerySectionContents);
+        }
+    }
+
+    $(document).on("click", "#pride-names-button", function(event) {
+        prideToggleSection('names');
+    });
+
+    $(document).on("click", "#pride-gallery-button", function(event) {
+        prideToggleSection('gallery');
+    });
+}
